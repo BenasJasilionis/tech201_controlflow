752e336227620805f75b5eba433eb7323179c5aa
BenasJasilionis <benas.jasilionis@gmail.com>
2023-01-31 17:09:54 +0000

    finished task 4.1

--- task4.1_summing_input_integers.py
+++ task4.1_summing_input_integers.py
@@ -2,14 +2,14 @@ user_inputs = []
 final_list = []
 count = 0
 while count < 10:
-    increment = int(input("Please type a number"))
-    user_inputs += increment
+    increment = input("Please type a number")
+    user_inputs.append(increment)
     count += 1
 
-print(len(user_inputs))
-print(user_inputs)
+#print(len(user_inputs))
+#print(user_inputs)
 for string in user_inputs:
     final_list.append(int(string))
-print(final_list)
+#print(final_list)
 list_sum = sum(final_list)
-print(list_sum / 10)
+print(f"The average value of the 10 you provided is: {list_sum / 10}")
